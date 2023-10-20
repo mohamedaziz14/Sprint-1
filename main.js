9a0d62ce50edd4417c6a2515dfce42c64a6165b7
Fawez11 <fawezfer12@gmail.com>
2023-10-20 12:51:01 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+// Variables:
+
+// Problem: Write a JavaScript function that declares a variable using var and returns its value.
+
+// Problem: Create a function that takes two arguments and checks if they are equal. Return true if they are and false if they are not.
+
+// Problem: Implement a function that declares a "constant" variable using var (even though var variables are not constant) and attempts to reassign it. Handle any resulting errors.
+
+// Problem: Create a function that defines a variable using var within its scope. Try to access the variable outside of the function and explain the result.
+
+// Problem: Write a function that takes a variable as an argument and returns its data type.
+
+// Conditional Statements:
+
+// Problem: Create a function that takes an age as an argument and returns a message based on whether the person is a child, a teenager, an adult, or a senior citizen.
+
+// Problem: Write a function that checks if a given number is even or odd and returns an appropriate message.
+
+// Problem: Create a function that simulates a basic login system. It takes a username and password as arguments and returns true if  "Login successful" if they match, or false if  "Login failed" if they don't.
+
+// Problem: Write a function that calculates the grade for a given test score. The function should return "A," "B," "C," "D," or "F" based on the score.
+
+// Problem: Create a function that takes a day of the week as an argument and returns whether it's a weekday or a weekend day.
