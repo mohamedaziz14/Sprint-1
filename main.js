d1dc51ef9b688c0a09e3121a8145159f7f14f13e
mohamedaziz14 <jo56501283@gmail.com>
2023-10-21 10:47:18 +0100

    90% done

--- main.js
+++ main.js
@@ -21,3 +21,55 @@
 // Problem: Write a function that calculates the grade for a given test score. The function should return "A," "B," "C," "D," or "F" based on the score.
 
 // Problem: Create a function that takes a day of the week as an argument and returns whether it's a weekday or a weekend day.
+///////////////exercice1
+var x=20
+function add(){
+    return x
+}
+///////////exercice2
+function equal(num1,num2){
+    if (num1===num2){
+    return true }
+    else {return false }
+
+
+}
+///////////////////exercice3
+function add(){
+    var x=2
+    return x=x+2
+}
+undefined
+add()
+4
+/////////////////ex4
+var x=2
+function add(){
+    return x}
+    //////////////ex5
+    function hello(x){
+        return typeof(x)
+          }
+          ///////////ex6
+          function age(a){
+            if (a<10){
+                return 'its a child'}
+                    else if (a>10 && a<18){
+                return 'its a teenager'}
+                    else if (a>18 && a<45 ){
+                return 'its an adult'}
+                    else {return 'its an senior citizen'}}
+                    /////////////////exercice7
+                    function hello1 (x){
+                        if (x%2===0){
+                            return 'even'}
+                        else {return 'odd'
+                             }
+                    }
+                    ////ex 8
+                    function login(username,passeword){
+                        if (username==='aziz'&& passeword===1234){
+                            return true}
+                        else{ return false}
+                    }
+                    ////////////ex9
